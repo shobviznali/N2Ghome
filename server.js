a24853e6eab1006f43ff11bf52d71f7953e5ad08
Ashot <110162000+shobviznali@users.noreply.github.com>
2023-12-24 01:51:49 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,36 +1,17 @@
 const express = require("express");
-const bodyParser = require('body-parser')
-const fs = require('fs')
-
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const router = require('./Routers/routerOrder')
 const app = express();
 
 app.use(bodyParser.json());
 app.use(express.json());
 
-app.listen(8000)
+app.get('/users', router);
+app.post('/users', router);
+app.put('/users', router);
+app.delete('/users/:id', router);
 
-app.get('/users', (req, res) => {
-    
-    const parsedData = JSON.parse(fs.readFileSync('data.json', "utf-8"));
-    res.json(parsedData)
+app.listen(8000, () => {
+    console.log("Server started at port 8000")
 })
-
-app.post('/users', (req, res) => {
-    
-    const jsonData = fs.readFileSync('data.json', 'utf-8');
-    const existingData = JSON.parse(jsonData);
-
-    const newUser = req.body;
-
-
-    existingData.push(newUser);
-
-    fs.writeFileSync('data.json', JSON.stringify(existingData, null, 2), 'utf-8');
-
-
-    res.json({ message: 'User added successfully' });
-
-    
-})
-// console.log();
-
